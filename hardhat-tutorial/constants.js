aa5d51fc8b360de9fe5b69487e1a7c81ed2a6b5b
Astrid Gaillard <astridgaillard@macbook-pro-de-astrid.home>
2022-06-27 12:05:12 +0200

    first commit

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xB3302069F47D599676Ac00ecC81604411ACb52e6";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
